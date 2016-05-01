b2cedc81fc6a38a2edbdd85932244f0ddc2d8ad1
jrice189 <jrice189@gmail.com>
2016-05-01 15:34:00 +0000

    Added MongoDB server connection

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+var mongo = require('mongodb');
 
-app.get('/', function(req, res) {
-  res.json('i am a beautiful butterfly');
+mongo.MongoClient.connect(process.env.MONGOLAB_URI || 'mongodb://0.0.0.0:27017/jrice-url', function(err, db) {
+    if (err) throw err;
+    else console.log('Successfully connected to MongoDB');
+    
+    app.get('/', function(req, res) {
+      res.type('text/plain');
+      res.send('i am a beautiful butterfly');
+    });
+    
+    app.listen(process.env.PORT || 8080);
 });
 
-app.listen(process.env.PORT || 4730);
