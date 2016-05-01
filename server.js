26c9085ac0db829a51d41964b4978f64df32a577
jrice189 <jrice189@gmail.com>
2016-05-01 15:41:14 +0000

    Changed connection parameters

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var mongo = require('mongodb');
+require('dotenv').config({
+    silent: true
+});
 
 mongo.MongoClient.connect(process.env.MONGOLAB_URI || 'mongodb://0.0.0.0:27017/jrice-url', function(err, db) {
     if (err) throw err;
@@ -11,6 +14,6 @@ mongo.MongoClient.connect(process.env.MONGOLAB_URI || 'mongodb://0.0.0.0:27017/j
       res.send('i am a beautiful butterfly');
     });
     
-    app.listen(process.env.PORT || 8080);
+    app.listen(process.env.PORT || 4037);
 });
 
