f16501f197040e35cc353b1b30f3d768f0c5625d
jrice189 <jrice189@gmail.com>
2016-07-23 20:34:00 +0000

    Improve redirect functionality

--- server.js
+++ server.js
@@ -5,17 +5,37 @@ require('dotenv').config({
     silent: true
 });
 
-var database;
+var sites;
 
 mongo.MongoClient.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/jrice-url', function(err, db) {
     if (err) console.log(err);
     else console.log('Successfully connected to MongoDB');
-    database=db;
+    sites=db.collection('sites');
     
     app.listen(process.env.PORT || 8080);
 });
 
-app.get('/', function(req, res) {
-    res.type('text/plain');
-    res.send('i am a beautiful butterfly');
+app.get('/new/:url', function(req, res) {
+    var ts=Date.now();
+    var s=req.params.url;
+    
+    sites.insert({id:ts, site:s},function(e,d){
+        if (e) throw e;
+        sites.find({id:ts}, function(err, cursor){
+            if (err) throw err;
+            cursor.next(function(e1, s){
+                if (e1) throw e1;
+                var jsonout={"original url": s['site'], "new url": s['id']};
+                res.send(jsonout);
+            });
+        });
+    });
+});
+
+app.get('/:url', function (req, res) {
+    var shortid=req.params.url;
+    sites.findOne({id:parseInt(shortid)}, function(err, d){
+        if (err) throw err;
+        res.redirect("http://"+d['site']);
+    });
 });
